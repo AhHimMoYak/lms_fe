f045963ce9bec1b32a516f4cb4c423af370d78d9
sora0319 <sojeoung536985@gmail.com>
2024-08-18 11:04:49 +0900

    feat : 라우터 기능 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './styles/index.css'
+import { BrowserRouter } from 'react-router-dom'
+import Render from './Render.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <BrowserRouter>
+        <Render />
+    </BrowserRouter>
+   
 )
