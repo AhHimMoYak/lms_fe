579c0c7e77a57b9192e41109580fb7224bb04a16
sora0319 <sojeoung536985@gmail.com>
2024-08-18 11:02:31 +0900

    feat : 주소값 라우팅 기능 추가

--- src/Render.jsx
+++ src/Render.jsx
@@ -1,11 +1,24 @@
+import { Routes, Route } from "react-router-dom"
+import { AuthChecker } from "./authentication/AuthChecker"
+import Body from "./pages/Body"
+import App from "./App"
+
 function Render() {
 
-    return (
-      <>
-        <h1>render page</h1>
-      </>
-    )
-  }
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<App/>}/>
+          <Route element={<AuthChecker/>}>
+            
+            <Route path="/test" element={<Body/>}/>
+          </Route>
+          
+        
+      </Routes>
+    </>
+  )
+}
   
   export default Render
   
