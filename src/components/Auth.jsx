a1e6467ec95b2e345e70ec1a2d1f5c31e080fa9d
Tiktokyi <tiktokyi@gmail.com>
2024-08-21 00:33:19 +0900

    mod : 로그인 버튼 클릭시 로그인 페이지 이동

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,11 +1,18 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Auth() {
   const [authorization, setAuthorization] = useState(null);
+  const naviagte = useNavigate();
+
   useEffect(() => {
     const token = localStorage.getItem("access");
     setAuthorization(token);
   }, []);
+
+  const onClickButton = () => {
+    naviagte("/login");
+  };
 
   return authorization ? (
     <>
@@ -13,7 +20,7 @@ function Auth() {
     </>
   ) : (
     <>
-      <button>로그인</button>
+      <button onClick={onClickButton}>로그인</button>
     </>
   );
 }
