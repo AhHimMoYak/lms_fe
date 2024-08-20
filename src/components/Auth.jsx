474be44a1cc5d78855000670b2a66906608dfd50
Tiktokyi <tiktokyi@gmail.com>
2024-08-21 00:20:47 +0900

    mod : 토큰 유무로 컴포넌트 값 출력

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,8 +1,18 @@
+import { useState, useEffect } from "react";
+
 function Auth() {
-  return (
+  const [authorization, setAuthorization] = useState(null);
+  useEffect(() => {
+    const token = localStorage.getItem("access");
+    setAuthorization(token);
+  }, []);
+
+  return authorization ? (
     <>
-      <h1>Auth입니다.</h1>
+      <h1>토큰이 있습니다.</h1>
     </>
+  ) : (
+    <h1>토큰이 없습니다.</h1>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import "../styles/Header.css";
+import Auth from "./Auth";
 
 function Header() {
   return (
     <>
       <div className="Header">
         <h1>헤더입니당~</h1>
+        <Auth />
       </div>
     </>
   );
