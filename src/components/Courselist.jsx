e870eff19ba64d5525a307b0d552963a6e166950
gomar <ju79318@naver.com>
2024-08-22 23:23:06 +0900

    mod : Courselist 컴포넌트 기본틀 생성

---
+++ src/components/Courselist.jsx
@@ -0,0 +1,57 @@
+import {useState, useEffect} from 'react';
+import Slider from 'react-slick';
+import '../styles/Courselist.css';
+
+// 테스트용 데이터
+const testCourses = [
+    {title: '강의1', instructor: '강사이름1'},
+    {title: '강의2', instructor: '강사이름1'},
+    {title: '강의3', instructor: '강사이름1'},
+    {title: '강의4', instructor: '강사이름1'},
+    {title: '강의5', instructor: '강사이름1'},
+    {title: '강의6', instructor: '강사이름1'},
+    {title: '강의7', instructor: '강사이름1'},
+
+];
+
+function Courselist() {
+    const [courses, setCourses] = useState([]);
+    const showMaxCnt = 4;
+
+    useEffect(() => {
+        const getCourses = async () => {
+            try {
+                // API 호출 대신 하드코딩된 데이터 사용
+                const courses = testCourses;
+                setCourses(courses);
+            } catch (error) {
+                console.error('Error fetching courses:', error);
+            }
+        };
+
+        getCourses()
+    }, []);
+
+    const sliderSettings = {
+        className: 'center',
+        infinite: false,
+        centerPadding: '60px',
+        swipeToSlide: true,
+        slidesToShow: showMaxCnt,
+    };
+
+    return (
+        <div className="slider-container">
+            <Slider {...sliderSettings}>
+                {courses.map((course, index) => (
+                    <div key={index} className="card">
+                        <h3>{course.title}</h3>
+                        <h3>{course.instructor}</h3>
+                    </div>
+                ))}
+            </Slider>
+        </div>
+    );
+}
+
+export default Courselist;
