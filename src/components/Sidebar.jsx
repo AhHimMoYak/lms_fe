64d9ef04239c8658abc9ca24e73485965793c364
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-08-23 11:38:26 +0900

    feat : 사이드바 구현

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import * as jose from "jose";
+import "../styles/Sidebar.css";
+
+// 토큰 디코딩 함수
+function decodeToken(token) {
+  try {
+    const claims = jose.decodeJwt(token);
+    console.log(claims);
+    return claims;
+  } catch (err) {
+    console.error("토큰 디코딩 실패:", err.message);
+    return null;
+  }
+}
+
+const Sidebar = ({ token, currentPage, setCurrentPage }) => {
+  const claims = decodeToken(token);
+
+  if (!claims) {
+    return <div>Error decoding token</div>;
+  }
+
+  const handleNavigation = (page) => {
+    setCurrentPage(page);
+  };
+
+  const { sub, email } = claims;
+
+  return (
+    <div className="sidebar">
+      <div className="top-spacing" />
+      <div className="profile">
+        <div className="profile-picture"></div>
+        <p className="name">{sub}</p>
+        <p className="email">{email}</p>
+        <hr className="divider" />
+      </div>
+      <div className="nav-buttons">
+        <button
+          className={`nav-button ${
+            currentPage === "dashboard" ? "active" : ""
+          }`}
+          onClick={() => handleNavigation("dashboard")}
+        >
+          <span className="icon">🏠</span> 대시보드
+        </button>
+        <button
+          className={`nav-button ${
+            currentPage === "courseList" ? "active" : ""
+          }`}
+          onClick={() => handleNavigation("courseList")}
+        >
+          <span className="icon">📚</span> 코스 목록
+        </button>
+        <button
+          className={`nav-button ${currentPage === "qa" ? "active" : ""}`}
+          onClick={() => handleNavigation("qa")}
+        >
+          <span className="icon">❓</span> Q&A
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
