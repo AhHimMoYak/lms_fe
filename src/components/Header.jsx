62f60ff4eeb2402e445774316d0f7bf131c3f3d3
tiktokyi <tiktokyi@gmail.com>
2024-08-23 19:09:16 +0900

    format : AuthButton import에 경로 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import AuthButton from "../components/AuthButton";
 
 function Header() {
   return (
@@ -28,7 +28,7 @@ function Header() {
             component="div"
             sx={{ flexGrow: 1 }}
           ></Typography>
-          <Button color="inherit">Login</Button>
+          <AuthButton />
         </Toolbar>
       </AppBar>
     </Box>
