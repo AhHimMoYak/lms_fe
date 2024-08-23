c2ac2b727478f1516514c18005f4a11d114c2488
Dowon <tnstjs1919@gmail.com>
2024-08-23 11:31:18 +0900

    feat : main 화면에 Banner, CourseButton 보이게 함

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,25 @@
-function Main(){
-    return(
-        <>
-            <h1>main</h1>
-        </>
-    )
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+import Banner from '../components/Banner';
+import CourseButton from '../components/CourseButton';
+
+function Main() {
+  const [selectedCourse, setSelectedCourse] = useState(null);
+
+  return (
+    <div>
+      <Banner />
+      <CourseButton onSelectCourse={setSelectedCourse} />
+
+      {/*코스별로 렌더링 될 내용*/}
+      {selectedCourse && (
+        <div style={{ marginTop: '20px', textAlign: 'center' }}>
+          <h2>{selectedCourse}</h2>
+          <p>Here is the content for {selectedCourse}.</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Main;
