87f37b82c2749a53d3f2051c9d652ae35d051781
tiktokyi <tiktokyi@gmail.com>
2024-08-24 19:09:58 +0900

    format : 불필요한 코드 제거

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,6 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import AuthButton from "../components/AuthButton";
 import { useNavigate } from "react-router-dom";
 
@@ -23,15 +21,6 @@ function Header() {
         <AppBar position="static">
           <Toolbar>
             <div><button onClick={handleRedirecToHome}>아힘모약</button></div>
-            {/* <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton> */}
             <Typography
               variant="h6"
               component="div"
