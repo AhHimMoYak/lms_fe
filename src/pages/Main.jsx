d984c01f7a6a49a57888491adeb2874f03f604f8
Dowon <tnstjs1919@gmail.com>
2024-08-24 16:03:29 +0900

    mod : onSelectCourse 에서 course id를 받아오게 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Banner from '../components/Banner';
 import CourseButton from '../components/CourseButton';
 
@@ -7,15 +6,15 @@ function Main() {
   const [selectedCourse, setSelectedCourse] = useState(null);
 
   return (
-    <div>
+    <div className="courselist">
       <Banner />
       <CourseButton onSelectCourse={setSelectedCourse} />
 
       {/*코스별로 렌더링 될 내용*/}
       {selectedCourse && (
         <div style={{ marginTop: '20px', textAlign: 'center' }}>
-          <h2>{selectedCourse}</h2>
-          <p>Here is the content for {selectedCourse}.</p>
+          <h2>{selectedCourse.categoryNumber}</h2>
+          <p>Here is the content for {selectedCourse.title}.</p>
         </div>
       )}
     </div>
