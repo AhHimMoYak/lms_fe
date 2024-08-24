a7359d6550e7565fa9199f859b5d7f7c31fe882d
tiktokyi <tiktokyi@gmail.com>
2024-08-24 18:57:25 +0900

    mod : 헤더에 홈이동 버튼 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,30 +8,40 @@ import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import AuthButton from "../components/AuthButton";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+  
+  const handleRedirecToHome = () => {
+    navigate("/");
+  }
+  
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1 }}
-          ></Typography>
-          <AuthButton />
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <div className="Header">
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <div><button onClick={handleRedirecToHome}>아힘모약</button></div>
+            {/* <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton> */}
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            ></Typography>
+            <AuthButton />
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
   );
 }
 
