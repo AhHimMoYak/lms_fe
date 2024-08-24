aab71db77a09a1fcd7e194ee2bcbd4f3d8de1380
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-08-24 18:51:31 +0900

    mod : Login.jsx 메소드들 삭제

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,42 +1,17 @@
 import { useEffect } from 'react'
 import Temp from '../components/Temp'
 import AuthManger from '../hooks/api/AuthManger'
+import LoginPage from '../components/LoginPage';
+
 
 function Login(){
-    const { Register, LogIn }= AuthManger();
-
-	const registerFetch = async () => {
-		try{
-			if(!localStorage.getItem("access")){
-				//console.log("start regiester");
-				//await Register();
-				console.log("start login");
-				await LogIn();
-			}
-			
-		} catch(error){
-			console.log(error);
-		}
-	}
-
-	useEffect(() =>{
-		registerFetch();
-	},[]);
-
-    function handleLogout(){
-        console.log("logout");
-        localStorage.removeItem("access");
-        localStorage.removeItem("refresh");
-    }
-
-
-    return(
-        <>
-            <h1>Login</h1>
-            <Temp/>
-            <button onClick={handleLogout}>Logout</button>
-        </>
+    return (
+    <>
+    <LoginPage/>
+    </>
     )
+
 }
+    
 
 export default Login;
