53b56624eef59478daefb7ac6cac1493b103b937
Dowon <tnstjs1919@gmail.com>
2024-08-24 16:00:54 +0900

    mod : selectedCourse 수정

--- src/components/CourseButton.jsx
+++ src/components/CourseButton.jsx
@@ -1,21 +1,14 @@
-// eslint-disable-next-line no-unused-vars
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from "react";
 import '../styles/CourseButton.css';
 import useAxios from "../hooks/api/useAxios.jsx";
 
-const CourseButton = () => {
-
+const CourseButton = ({onSelectCourse}) => {
     const [buttonNames, setButtonNames] = useState([]);
-    const { fetchData , data , error } = useAxios();
+    const {fetchData, data, error} = useAxios();
 
     useEffect(() => {
         fetchData("/category", "get");
-
     }, []);
-
-    const handleClick = (index) => {
-        setSelectedButton(index);
-    };
 
     useEffect(() => {
         if (Array.isArray(data)) {
@@ -23,28 +16,27 @@ const CourseButton = () => {
         }
     }, [data]);
 
-
-    const [selectedButton, setSelectedButton] = useState(null);
+    const handleClick = (index) => {
+        const selectedCourse = buttonNames[index] || {categoryNumber: 'Unknown', title: 'Unknown'};
+        onSelectCourse(selectedCourse);
+    };
 
 
     return (
         <div className="CourseButton">
+            {buttonNames.map((item, index) => (
+                <button
+                    key={index}
+                    onClick={() => handleClick(index)}
+                    className={`course-button`}
+                >
+                    {item.title}
+                </button>
 
-            { buttonNames.map((item, index) => (
-                    <button
-                        key={index}
-                        onClick={() => handleClick(index)}
-                        className={`course-button ${selectedButton === index ? 'selected' : ''}`}
-                    >
-                        {item.title}
-                    </button>
-
-    ))
+            ))
             };
         </div>
     );
 };
-
-
 
 export default CourseButton;
