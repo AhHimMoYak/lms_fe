74e1e26c257f01bd823a9f586aa9bf71415d7506
gomar <ju79318@naver.com>
2024-08-25 11:50:47 +0900

    mod : Courselist 컴포넌트 태그 수정

--- src/components/Courselist.jsx
+++ src/components/Courselist.jsx
@@ -1,34 +1,24 @@
 import {useState, useEffect} from 'react';
 import Slider from 'react-slick';
 import '../styles/Courselist.css';
-import javaImage from '../assets/java사진.jpg';
-
-// 테스트용 데이터
-const testCourses = [
-    {title: '강의1', instructor: '강사이름1', image: javaImage},
-    {title: '강의2', instructor: '강사이름2', image: javaImage},
-    {title: '강의3', instructor: '강사이름3', image: null},
-    {title: '강의4', instructor: '강사이름4', image: javaImage},
-    {title: '강의5', instructor: '강사이름5', image: null},
-    {title: '강의6', instructor: '강사이름6', image: null},
-];
+import useAxios from "../hooks/api/useAxios.jsx";
 
 function Courselist() {
     const [courses, setCourses] = useState([]);
+    const {fetchData, data} = useAxios();
     const showMaxCnt = 5;
 
     useEffect(() => {
-        const getCourses = async () => {
-            try {
-                const courses = testCourses;
-                setCourses(courses);
-            } catch (error) {
-                console.error('Error fetching courses:', error);
-            }
-        };
-
-        getCourses();
+        fetchData("/enrollment/course", "get");
     }, []);
+
+    useEffect(() => {
+        console.log(data);
+        if (Array.isArray(data)) {
+            setCourses(data);
+        }
+    }, [data]);
+
 
     const sliderSettings = {
         infinite: false,
@@ -47,24 +37,26 @@ function Courselist() {
                                 <img className="course_image" src={course.image}/>
                                 <div className="text-container">
                                     <h3 className="image_title">{course.title}</h3>
-                                    <h3 className="image_instructor">{course.instructor}</h3>
+                                </div>
+                                <div className="instructor-container">
+                                    <h3 className="image_instructor">{course["instructor"]}</h3>
                                 </div>
                             </>
                         ) : (
                             <>
                                 <div className="text-container">
                                     <h3 className="image_title">{course.title}</h3>
-                                    <h3 className="image_instructor">{course.instructor}</h3>
+                                </div>
+                                <div className="instructor-container">
+                                    <h3 className="image_instructor">{course["instructor"]}</h3>
                                 </div>
                             </>
-                        )
-                        }
+                        )}
                     </div>
                 ))}
             </Slider>
         </div>
-    )
-        ;
+    );
 }
 
 export default Courselist;
