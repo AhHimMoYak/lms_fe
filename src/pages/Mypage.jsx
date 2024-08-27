4e5d10b1fc9c7fc59933ae479c00cc04bae7926d
sora0319 <sojeoung536985@gmail.com>
2024-08-27 11:10:14 +0900

    fix : return 빈태그가 없어서 생기는 오류 해결 및 routing을 위한 outlet 설정

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,33 +1,28 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { Outlet } from "react-router-dom";
 
 function Mypage() {
   const token = localStorage.getItem("access");
 
   const [currentPage, setCurrentPage] = useState("dashboard");
 
-  const renderPageContent = () => {
-    switch (currentPage) {
-      case "dashboard":
-        return <h1>대시보드</h1>;
-      case "courseList":
-        return <h1>코스 목록</h1>;
-      case "qa":
-        return <h1>Q&A</h1>;
-      default:
-        return <h1>대시보드</h1>;
-    }
-  };
-
   return (
-    <div className="app-container">
+    <Fragment>
+      <Header/>
+      <div className="app-container">
       <Sidebar
         token={token}
         currentPage={currentPage}
         setCurrentPage={setCurrentPage}
       />
-      <div className="main-content">{renderPageContent()}</div>
+      <div className="main-content"><Outlet/></div>
     </div>
+    <Footer/>
+    </Fragment>
+    
   );
 }
 
