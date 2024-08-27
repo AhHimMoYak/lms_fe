72e718ac17388a3cc7c07fea93798742a1bd8e08
sora0319 <sojeoung536985@gmail.com>
2024-08-27 19:30:24 +0900

    feat : mypage 각 타이틀 배너 생성

---
+++ src/components/Title.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Title(){
+
+    const [name, setName] = useState("대시보드");
+    const params = useParams();
+    console.log(params);
+
+    useEffect(() => {
+        console.log(location.pathname);
+        if(location.pathname === "/mypage" || location.pathname === "/mypage/dashboard"){
+            setName("대시보드");
+        }
+        if(location.pathname === "/mypage/course"){
+            setName("코스목록");
+        }
+        if(location.pathname === "/mypage/qna"){
+            setName("QnA");
+        }
+        if(location.pathname === `/mypage/course/${params.courseId}`){
+            setName("강의상세");
+        }
+    },[location.pathname]);
+
+    return(
+        <div className="title-content">
+            <h3>아힘모약</h3>
+            <h1>{name}</h1>
+        </div>
+    )
+}
+
+export default Title;
