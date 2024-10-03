07aec305ffce9f392f7bf9652aa3a0a33cc27ab6
sora0319 <sojeoung536985@gmail.com>
2024-10-04 06:25:46 +0900

    feat : 새로운 Render 추가 및 이전 Render 파일 생성

--- src/Render.jsx
+++ src/Render.jsx
@@ -1,36 +1,12 @@
 import { Routes, Route } from "react-router-dom"
 import { AuthChecker } from "./authentication/AuthChecker"
 import Body from "./pages/Body"
-import App from "./App"
-import Main from "./pages/Main"
-import Mypage from "./pages/Mypage"
-import Login from "./pages/Login"
-import Register from "./pages/Register";
-import RegistrationCompletion from "./pages/RegistrationCompletion.jsx";
-import Courselist from "./components/Courselist.jsx"
-import CourseBoard from "./components/CourseBoard.jsx"
-import Curriculum from "./components/Curriculum.jsx"
-import Stream from "./pages/Stream.jsx";
 
 function Render() {
 
   return (    
     <Routes>
-		<Route path="/" element={<Main/>}/>
-		<Route path="/login" element={<Login/>}/>
-		<Route path="/join" element={<Register/>}/>
-		<Route path="/join/complete" element={<RegistrationCompletion/>}/>
-		<Route element={<AuthChecker/>}>
-			<Route path="/mypage/*" element={<Mypage/>}>
-				<Route path="" element={<Courselist/>}/>
-				<Route path="dashboard" element={<Courselist/>}/>
-				<Route path="course" element={<CourseBoard/>}/>
-				<Route path="qna" element={<></>}/>
-				<Route path="course/:courseId" element={<Curriculum/>}/>
-			</Route>
-			<Route path="/stream" element={<Stream/>}/>
-			<Route path="/test" element={<Body/>}/>
-		</Route>
+		
     
     </Routes>
   )
