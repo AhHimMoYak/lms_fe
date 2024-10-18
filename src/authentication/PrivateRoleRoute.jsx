40a84677706da82f45007bfc1faf0e572d14a311
sora0319 <sojeoung536985@gmail.com>
2024-10-18 13:22:44 +0900

    fix : react-router-dom에 따른 Redirect 사용 금지 해결 및 jwtDecode error 해결

--- src/authentication/PrivateRoleRoute.jsx
+++ src/authentication/PrivateRoleRoute.jsx
@@ -1,15 +1,15 @@
-import { Outlet, Redirect } from "react-router-dom";
-import jwtDecode from "jwt-decode";
+import { Outlet, Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 export const PrivateRoleRoute = (roles) => {
   const token = localStorage.getItem("access");
 
   if (!token) {
-    return <Redirect to="/" />;
+    return <Navigate to="/" />;
   }
 
   const decodedToken = jwtDecode(token);
   const userRole = decodedToken.role;
 
-  return roles.includes(userRole) ? <Outlet /> : <Redirect to="/" />;
+  return roles.includes(userRole) ? <Outlet /> : <Navigate to="/" />;
 };
