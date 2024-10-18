581ef66f8f7100a041bfeb209f6f5ccd21e2abae
sora0319 <sojeoung536985@gmail.com>
2024-10-18 12:21:45 +0900

    feat : token이 없을 때 header에 추가하지 않도록 변환

--- src/authentication/axios/Axios.jsx
+++ src/authentication/axios/Axios.jsx
@@ -1,58 +1,59 @@
-import axios from 'axios';
-import { API_BASE_URL } from '../../api-config';
+import axios from "axios";
+import { API_BASE_URL } from "../../api-config";
 
 export const Axios = () => {
-    let accessToken = JSON.parse(localStorage.getItem("access"));
-    const refreshToken = JSON.parse(localStorage.getItem("refresh"));
-    const source = axios.CancelToken.source();
+  let accessToken = JSON.parse(localStorage.getItem("access"));
+  const refreshToken = JSON.parse(localStorage.getItem("refresh"));
+  const source = axios.CancelToken.source();
 
-    const axiosInstance = axios.create({
-        baseURL : API_BASE_URL,
-        headers: { Authorization: `[Bearer]${accessToken}` }
-    });
+  const axiosInstance = axios.create({
+    baseURL: API_BASE_URL,
+    headers: { "Content-Type": "application/json" },
+  });
 
-    axiosInstance.interceptors.request.use(async (req) => {
+  if (accessToken === null) return axiosInstance;
+
+  axiosInstance.interceptors.request.use(async (req) => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/accesstoken`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `[Bearer]${accessToken}`,
+        },
+        cancelToken: source.token,
+      });
+
+      if (response.msg !== "OK") return req;
+
+      if (response.msg === "INVALID") {
         try {
-            const response = await axios.get(`${API_BASE_URL}/accesstoken`, {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `[Bearer]${accessToken}`
-                },
-                cancelToken: source.token,
-            });
+          const response = await axios.get(`${API_BASE_URL}/refreshtoken`, {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `[Bearer]${refreshToken}`,
+            },
+            cancelToken: source.token,
+          });
 
-            if (response.msg !== "OK") return req;
-
-        
-            if(response.msg === "INVALID"){
-                try {
-                    const response = await axios.get(`${baseURL}/refreshtoken`, {
-                        headers: {
-                            "Content-Type": "application/json",
-                            Authorization: `[Bearer]${refreshToken}`
-                        },
-                        cancelToken: source.token,
-                    });
-
-                    if (response.status === 200){
-                        const token = response.headers['authorization'].split(']');
-                        accessToken = token[1];
-                        localStorage.setItem("access", JSON.stringify(accessToken));
-                        req.headers.Authorization = `[Bearer]${accessToken}`;
-                    }
-                } catch (error) {
-                    localStorage.removeItem("access");
-                    localStorage.removeItem("refresh");
-                    window.location.href = "/";
-                }
-            }
-            return req;
+          if (response.status === 200) {
+            const token = response.headers["authorization"].split("]");
+            accessToken = token[1];
+            localStorage.setItem("access", JSON.stringify(accessToken));
+            req.headers.Authorization = `[Bearer]${accessToken}`;
+          }
         } catch (error) {
-            localStorage.removeItem("access");
-            localStorage.removeItem("refresh");
-            throw error;
+          localStorage.removeItem("access");
+          localStorage.removeItem("refresh");
+          window.location.href = "/";
         }
-        });
+      }
+      return req;
+    } catch (error) {
+      localStorage.removeItem("access");
+      localStorage.removeItem("refresh");
+      throw error;
+    }
+  });
 
-    return axiosInstance;
+  return axiosInstance;
 };
