fa86010534af8fc41c443366c4766eb63cafb744
sora0319 <sojeoung536985@gmail.com>
2024-10-21 09:11:49 +0900

    fix : 사용자 이름 수정

--- src/Render.jsx
+++ src/Render.jsx
@@ -3,17 +3,15 @@ import { AuthChecker } from "./authentication/AuthChecker";
 import Mypage from "./pages/Mypage";
 import { PrivateRoleRoute } from "./authentication/PrivateRoleRoute";
 import BasicFrame from "./components/Main/BasicFrame";
-import Main from "./pages/Main";
+
 import CourseFrame from "./components/Main/CourseFrame";
+import Main from "./pages/Main/Main";
 import Video from "./pages/Main/Video";
 import Live from "./pages/Main/Live";
 import IntrodcutionFrame from "./components/Main/IntroductionFrame";
 import VideoExplaine from "./pages/Main/VideoExplaine";
 import LiveExplaine from "./pages/Main/VideoExplaine copy";
-import MypageFrame from "./components/Employee/MypageFrame";
-
 import Login from "./pages/Login";
-import Body from "./pages/Body";
 import EmployeeRouter from "./EmployeeRender";
 import SupervisorRoutes from "./SupervisorRoutes";
 
@@ -22,7 +20,7 @@ function Render() {
     <Routes>
       {/* 기존 메인 페이지 */}
       <Route element={<BasicFrame />}>
-        <Route path="/" element={<Body />} />
+        <Route path="/" element={<Main />} />
         <Route path="/course/*" element={<CourseFrame />}>
           <Route path="video" element={<Video />} />
           <Route path="live" element={<Live />} />
@@ -39,7 +37,7 @@ function Render() {
         {/* 마이페이지 공통 사용 부분 */}
         <Route path="mypage/*" element={<Mypage />}>
           {/* 역할에 따른 페이지 구성 분리 및 접근 제한 */}
-          <Route element={PrivateRoleRoute("EMPLOYEE")}>
+          <Route element={PrivateRoleRoute("COMPANY")}>
             <Route path="*" element={<EmployeeRouter />} />
           </Route>
           <Route element={PrivateRoleRoute("SUPERVISOR")}>
@@ -49,7 +47,6 @@ function Render() {
           <Route element={PrivateRoleRoute("MANAGER")}></Route>
 
           <Route element={PrivateRoleRoute("INSTRUCTOR")}></Route>
-          
         </Route>
       </Route>
     </Routes>
