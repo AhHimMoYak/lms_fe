c4df5c267b3c80fa7997ae77602ff07ea68abeff
Dowon <tnstjs1919@gmail.com>
2024-10-23 21:05:41 +0900

    style : companyDetailed css 생성

--- src/pages/Supervisor/CompanyDetailed.jsx
+++ src/pages/Supervisor/CompanyDetailed.jsx
@@ -1,10 +1,31 @@
+import {useEffect} from "react";
+import useAxios from "../../hooks/api/useAxios.jsx";
+
+import '/src/styles/CompanyDetailed.css'
+
 function CompanyDetailed() {
-  return (
-    <div
-      style={{
-        backgroundColor: "purple",
-      }}
-    ></div>
+
+    const {data: companyInfo, error, fetchData: fetchCompanyInfo} = useAxios();
+    const {data: companyId, fetchData: fetchCompanyIdData} = useAxios();
+
+    // 1. 회사 id 가져오기
+    useEffect(() => {
+        fetchCompanyIdData("/users/companyId", "GET");
+    }, []);
+
+    // 2. 가져온 회사 id 로 회사 정보 가져오기
+    useEffect(() => {
+        if (companyId != null) {
+            const id = companyId.id;
+            fetchCompanyInfo(``)
+        }
+    }, []);
+
+    return (
+      <div>
+          <h2> 회사정보조회</h2>
+      </div>
+
   );
 }
 
