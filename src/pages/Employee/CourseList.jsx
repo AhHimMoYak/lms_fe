794e4cc0e97267c91ba6bb4bfab5a183e884a013
Dowon <tnstjs1919@gmail.com>
2024-10-24 13:47:37 +0900

    feat : 마이페이지의 수강중인 코스 리스트 출력

--- src/pages/Employee/CourseList.jsx
+++ src/pages/Employee/CourseList.jsx
@@ -1,12 +1,57 @@
+import {useEffect} from "react";
+
+import '/src/styles/CourseListCss.css'
+import useAxios from "../../hooks/api/useAxios.jsx";
+import {useLocation} from "react-router-dom";
+
 function CourseList() {
+    const query = new URLSearchParams(useLocation().search);
+    const page = parseInt(query.get("page")) || 1;
+    const {data: courseData, error, fetchData: fetchUserCourse} = useAxios();
+
+    useEffect(() => {
+        fetchUserCourse(`/course/myPage?page=${page}&size=6`, "GET");
+    }, [page]);
+
+    useEffect(() => {
+        if (courseData) {
+            console.log(courseData);
+        }
+    }, [courseData]);
+
+    const course = courseData?.content ?? [];
+
   return (
-    <div
-      style={{
-        backgroundColor: "purple",
-      }}
-    >
-      CourseList
-    </div>
+      <div className="mycourse-list">
+          <h3>수강 중인 코스</h3>
+          <table className="mycourse-list-table">
+              <thead>
+              <tr>
+                  <td>No.</td>
+                  <th>코스 제목</th>
+                  <th>카테고리</th>
+                  <th>강사</th>
+              </tr>
+              </thead>
+
+              <tbody>
+              {course && course.length > 0 ? (
+                  course.map((courseItem, index) => (
+                      <tr key={index}>
+                          <td>No.{courseItem.id}</td>
+                          <td>{courseItem.title}</td>
+                          <td>{courseItem.category || "N/A"}</td>
+                          <td>{courseItem.tutorName || "N/A"}</td>
+                      </tr>
+                  ))
+              ) : (
+                  <tr>
+                      <td colSpan="5">수강 중인 코스가 없습니다.</td>
+                  </tr>
+              )}
+              </tbody>
+          </table>
+      </div>
   );
 }
 
