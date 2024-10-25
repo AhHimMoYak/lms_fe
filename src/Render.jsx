8a7f7ccee77cf5b3507a88cc55973757e11844f5
nowgnodeel123 <nowgnodeel123@gmail.com>
2024-10-25 18:49:12 +0900

    add : 수정페이지, 패스워드확인페이지 import 추가

--- src/Render.jsx
+++ src/Render.jsx
@@ -15,43 +15,49 @@ import EmployeeRouter from "./EmployeeRender";
 import SupervisorRoutes from "./SupervisorRoutes";
 import LiveStream from "./pages/Employee/LiveStream.jsx";
 import VideoExplaine from "./pages/Main/VideoExplaine.jsx";
+import Register from "./pages/Register.jsx";
+import UpdateUser from "./pages/UpdateUser";
+import PasswordPrompt from "./pages/PasswordPrompt";
 
 function Render() {
-  return (
-    <Routes>
-      {/* 기존 메인 페이지 */}
-      <Route element={<BasicFrame />}>
-        <Route path="/" element={<Main />} />
-        <Route path="/course/*" element={<CourseFrame />}>
-          <Route path="video" element={<Video />} />
-          <Route path="live" element={<Live />} />
-        </Route>
-        <Route path="/introduction/*" element={<IntrodcutionFrame />}>
-          <Route path="video/:courseId" element={<VideoExplaine />} />
-          <Route path="live/:courseId" element={<LiveExplaine />} />
-        </Route>
-        <Route path="/live/:streamKey" element={<LiveStream />}>
-        </Route>
-      </Route>
-      <Route path="/login" element={<Login />} />
+    return (
+        <Routes>
+            {/* 기존 메인 페이지 */}
+            <Route element={<BasicFrame/>}>
+                <Route path="/" element={<Main/>}/>
+                <Route path="/course/*" element={<CourseFrame/>}>
+                    <Route path="video" element={<Video/>}/>
+                    <Route path="live" element={<Live/>}/>
+                </Route>
+                <Route path="/introduction/*" element={<IntrodcutionFrame/>}>
+                    <Route path="video/:courseId" element={<VideoExplaine/>}/>
+                    <Route path="live/:courseId" element={<LiveExplaine/>}/>
+                </Route>
+                <Route path="/live/:streamKey" element={<LiveStream/>}>
+                </Route>
+            </Route>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
+            <Route path="/passwordPrompt" element={<PasswordPrompt/>}/>
+            <Route path="/updateUser" element={<UpdateUser/>}/>
 
-      {/* 로그인 사용자 구분 */}
-      <Route element={<AuthChecker />}>
-        {/* 마이페이지 공통 사용 부분 */}
-        <Route path="mypage/*" element={<Mypage />}>
-          <Route path="*" element={<EmployeeRouter />} />
-          <Route path="*" element={<SupervisorRoutes />} />
-          {/* 역할에 따른 페이지 구성 분리 및 접근 제한 */}
-          <Route element={PrivateRoleRoute("COMPANY")}></Route>
-          <Route element={PrivateRoleRoute("SUPERVISOR")}></Route>
+            {/* 로그인 사용자 구분 */}
+            <Route element={<AuthChecker/>}>
+                {/* 마이페이지 공통 사용 부분 */}
+                <Route path="mypage/*" element={<Mypage/>}>
+                    <Route path="*" element={<EmployeeRouter/>}/>
+                    <Route path="*" element={<SupervisorRoutes/>}/>
+                    {/* 역할에 따른 페이지 구성 분리 및 접근 제한 */}
+                    <Route element={PrivateRoleRoute("COMPANY")}></Route>
+                    <Route element={PrivateRoleRoute("SUPERVISOR")}></Route>
 
-          <Route element={PrivateRoleRoute("MANAGER")}></Route>
+                    <Route element={PrivateRoleRoute("MANAGER")}></Route>
 
-          <Route element={PrivateRoleRoute("INSTRUCTOR")}></Route>
-        </Route>
-      </Route>
-    </Routes>
-  );
+                    <Route element={PrivateRoleRoute("INSTRUCTOR")}></Route>
+                </Route>
+            </Route>
+        </Routes>
+    );
 }
 
 export default Render;
