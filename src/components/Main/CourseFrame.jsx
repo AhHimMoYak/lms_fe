e0afc844432e8b37044e49852d7b7873a1d26367
sora0319 <sojeoung536985@gmail.com>
2024-10-28 05:59:49 +0900

    feat : tab 요소 추가

--- src/components/Main/CourseFrame.jsx
+++ src/components/Main/CourseFrame.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import CourseSidebar from "./CourseSidebar";
 
 function CourseFrame() {
@@ -9,7 +9,17 @@ function CourseFrame() {
 
   return (
     <div style={containerStyle}>
-      <Outlet />
+      <h2>나의 코스</h2>
+      <div className="tabs">
+        <NavLink to="contents" className={({ isActive }) => (isActive ? 'active' : '')}>탭 1</NavLink>
+        <NavLink to="tab2" className={({ isActive }) => (isActive ? 'active' : '')}>탭 2</NavLink>
+        <NavLink to="tab3" className={({ isActive }) => (isActive ? 'active' : '')}>탭 3</NavLink>
+      </div>
+
+      {/* 중첩된 경로의 컴포넌트를 렌더링할 Outlet */}
+      <div className="tab-content">
+        <Outlet />
+      </div>
     </div>
   );
 }
