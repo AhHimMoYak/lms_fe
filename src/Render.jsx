2910046dba533cb36ced580434b8c9bcb1b3f8d3
nowgnodeel123 <nowgnodeel123@gmail.com>
2024-10-29 15:52:57 +0900

    refactor : MediaUpload 라우팅 추가

--- src/Render.jsx
+++ src/Render.jsx
@@ -1,5 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-import { AuthChecker } from "./authentication/AuthChecker";
+import {Routes, Route} from "react-router-dom";
+import {AuthChecker} from "./authentication/AuthChecker";
 import BasicFrame from "./components/BasicFrame.jsx";
 
 import CourseFrame from "./components/Mypage/CourseFrame.jsx";
@@ -18,34 +18,35 @@ import CourseDetailed from "./pages/Mypage/CourseDetailed.jsx";
 import LiveDetail from "./pages/Mypage/LiveDetail.jsx";
 import QnAList from "./pages/Mypage/QnAList.jsx";
 import QnATotalList from "./pages/Mypage/QnATotalList.jsx";
+import MediaUpload from "./pages/Mypage/MediaUpload.jsx";
 
 function Render() {
     return (
         <Routes>
-            <Route path="/signin" element={<Login />} />
-            <Route path="/signup" element={<Register />} />
+            <Route path="/signin" element={<Login/>}/>
+            <Route path="/signup" element={<Register/>}/>
 
-            <Route element={<BasicFrame />}>
-                <Route path="/" element={<Main />} />
-                <Route path="course" element={<Video />} />
-                <Route path="/course/:courseId" element={<VideoExplain />} />
-                <Route path="/live/:streamKey" element={<LiveStream />} />
+            <Route element={<BasicFrame/>}>
+                <Route path="/" element={<Main/>}/>
+                <Route path="course" element={<Video/>}/>
+                <Route path="/course/:courseId" element={<VideoExplain/>}/>
+                <Route path="/live/:streamKey" element={<LiveStream/>}/>
 
                 {/* <Route element={<AuthChecker />}> */}
-                <Route path="mypage/*" element={<MypageFrame />}>
-                    <Route path="" element={<Dashboard />} />
-                    <Route path="dashboard" element={<Dashboard />} />
-                    <Route path="courses" element={<CourseList />} />
-                    <Route path="course/:courseId" element={<CourseFrame />}>
-                        <Route path="" element={<CourseDetailed />} />
-                        <Route path="contents" element={<CourseDetailed />} />
-                        <Route path="live" element={<LiveDetail />} />
-                        <Route path="qna" element={<QnAList />} />
+                <Route path="mypage/*" element={<MypageFrame/>}>
+                    <Route path="" element={<Dashboard/>}/>
+                    <Route path="dashboard" element={<Dashboard/>}/>
+                    <Route path="courses" element={<CourseList/>}/>
+                    <Route path="course/:courseId" element={<CourseFrame/>}>
+                        <Route path="" element={<CourseDetailed/>}/>
+                        <Route path="contents" element={<CourseDetailed/>}/>
+                        <Route path="live" element={<LiveDetail/>}/>
+                        <Route path="qna" element={<QnAList/>}/>
                     </Route>
-                    <Route path="qna" element={<QnATotalList />} />
-
-                    <Route path="passwordPrompt" element={<PasswordPrompt />} />
-                    <Route path="updateUser" element={<UpdateUser />} />
+                    <Route path="qna" element={<QnATotalList/>}/>
+                    <Route path=":courseId/uploadMedia/:curriculumId" element={<MediaUpload/>}/>
+                    <Route path="passwordPrompt" element={<PasswordPrompt/>}/>
+                    <Route path="updateUser" element={<UpdateUser/>}/>
                 </Route>
                 {/* </Route> */}
             </Route>
