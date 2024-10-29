dce044e176c2739a5110a148b1af180b3af9c95b
Dowon <tnstjs1919@gmail.com>
2024-10-29 16:46:01 +0900

    refector : api 수정, 코드 리팩토리

--- src/pages/Mypage/Dashboard.jsx
+++ src/pages/Mypage/Dashboard.jsx
@@ -5,31 +5,19 @@ import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
     const { data, fetchData } = useAxios();
-    const { data: qnaBoardData, fetchData: fetchBoardData } = useAxios();
     const navigate = useNavigate();
 
     useEffect(() => {
-        fetchData(`/course/myPage?page=1&size=4`, "GET");
+        fetchData(`/course`, "GET");
     }, []);
 
-    useEffect(() => {
-        fetchBoardData("/courseBoard/myBoard", "GET");
-    }, []);
-
-    useEffect(() => {
-        if (qnaBoardData) {
-            console.log(qnaBoardData.boards.courseId);
-        }
-    }, []);
 
     const clickDetailCourse = (courseId) => {
         navigate(`/mypage/course/${courseId}`);
     };
-    const clickDetailBoard = (courseId, courseBoardId) => {
-        navigate(`/mypage/course/${courseId}/qna/${courseBoardId}`);
-    };
+
     const clickListCourse = () => {
-        navigate("/mypage/applied");
+        navigate("/mypage/courses");
     };
 
     return (
@@ -41,7 +29,7 @@ function Dashboard() {
                 </button>
             </div>
             <div className="course-cards-container">
-                {data?.content?.map((course) => (
+                {data?.map((course) => (
                     <div
                         key={course.id}
                         className="course-card"
@@ -56,51 +44,6 @@ function Dashboard() {
                         </div>
                     </div>
                 ))}
-            </div>
-            <div className="qna-list-container">
-                <div className="dashboard-header">
-                    <h2 className="dashboard-title">작성한 QnA</h2>
-                </div>
-                <table className="write-qna-table">
-                    <thead>
-                        <tr>
-                            <th>제목</th>
-                            <th>코스명</th>
-                            <th>작성일</th>
-                            <th>답변</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {qnaBoardData?.boards?.map((qna) => (
-                            <tr
-                                key={qna.boardId}
-                                className="qna-table-row"
-                                onClick={() =>
-                                    clickDetailBoard(qna.courseId, qna.boardId)
-                                }
-                            >
-                                <td className="qna-title">{qna.title}</td>
-                                <td className="qna-course">{qna.courseName}</td>
-                                <td className="qna-created">
-                                    {
-                                        new Date(qna.createdAt)
-                                            .toISOString()
-                                            .split("T")[0]
-                                    }
-                                </td>
-                                <td
-                                    className={
-                                        qna.commitCount > 0
-                                            ? "qna-answered"
-                                            : "qna-not-answered"
-                                    }
-                                >
-                                    {qna.commitCount > 0 ? "완료" : "미답변"}
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
             </div>
         </div>
     );
