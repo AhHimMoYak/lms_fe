a68fb7fec41b78812587602fcfd0f21c4f2bc8cb
sora0319 <sojeoung536985@gmail.com>
2024-10-30 18:15:14 +0900

    feat : 동영상 및 자료 다운로드 기능 구현

--- src/pages/Mypage/CourseDetailed.jsx
+++ src/pages/Mypage/CourseDetailed.jsx
@@ -1,11 +1,16 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import useAxios from "../../hooks/api/useAxios.jsx";
 import { useNavigate, useParams } from "react-router-dom";
 import "../../styles/Mypage/CourseDetailed.css";
+import { Download, Play } from "react-feather";
 
 function CourseDetailed() {
     const { data, fetchData } = useAxios();
+    const { data: contentData, fetchData: contentFetchData } = useAxios();
     const { courseId } = useParams();
+    const [curriculumId, setCurriculumId] = useState(null);
+    const [contentId, setContentId] = useState(null);
+    const [contentType, setcontentType] = useState(null);
     const [activeCurriculum, setActiveCurriculum] = useState([]);
     const navigate = useNavigate();
     useEffect(() => {
@@ -13,16 +18,47 @@ function CourseDetailed() {
     }, [courseId]);
 
     const toggleCurriculum = (index) => {
-        setActiveCurriculum((prevState) =>
-            prevState.includes(index)
-                ? prevState.filter((i) => i !== index)
-                : [...prevState, index]
-        );
+        setActiveCurriculum((prevState) => (prevState.includes(index) ? prevState.filter((i) => i !== index) : [...prevState, index]));
     };
 
-    const clickContent = () => {
-        navigate("/mypage/course"); //임시
+    console.log(data);
+
+    const clickContent = ({ curriculumId, contentId, contentType }) => {
+        setCurriculumId(curriculumId);
+        setContentId(contentId);
+        setcontentType(contentType);
     };
+
+    useEffect(() => {
+        console.log(curriculumId);
+        if (curriculumId && contentId) {
+            console.log(curriculumId);
+            contentFetchData(`course/${curriculumId}/curriculum/${curriculumId}/contents/${contentId}`, "get");
+        }
+    }, [curriculumId, contentId]);
+
+    useEffect(() => {
+        if (contentData) {
+            console.log(contentData.fileInfo);
+            if (contentType === "MATERIAL") {
+                const downloadLink = document.createElement("a");
+                downloadLink.href = `http://localhost:8080/file/material/${contentData.fileInfo}`; // 서버의 파일 URL
+                downloadLink.download = "meterial"; // 파일명 지정 (서버에서 받은 파일명 사용 가능)
+
+                // a 태그 클릭 이벤트 트리거로 다운로드 시작
+                document.body.appendChild(downloadLink);
+                downloadLink.click();
+
+                // 다운로드 후 a 태그 제거
+                document.body.removeChild(downloadLink);
+
+                //navigate(`/pdf?info=${contentData.fileInfo}`);
+            }
+            if (contentType === "VIDEO") {
+                navigate(`/stream/video?info=${contentData.fileInfo}`);
+            }
+        }
+    }, [contentData]);
 
     if (!data) {
         return <p>No course data available.</p>;
@@ -44,47 +80,50 @@ function CourseDetailed() {
                         </thead>
                         <tbody>
                             {data.curriculumList.map((curriculum, index) => (
-                                <>
-                                    <tr
-                                        key={curriculum.id}
-                                        onClick={() => toggleCurriculum(index)}
-                                        className="curriculum-title"
-                                    >
+                                <React.Fragment key={curriculum.id}>
+                                    <tr key={curriculum.id} onClick={() => toggleCurriculum(index)} className="curriculum-title">
                                         <td>
                                             <div className="curriculum-title-wrapper">
                                                 <span>{curriculum.title}</span>
-                                                <span className="curriculum-show">
-                                                    {activeCurriculum.includes(
-                                                        index
-                                                    )
-                                                        ? " ▼"
-                                                        : " ▶"}
-                                                </span>
+                                                <span className="curriculum-show">{activeCurriculum.includes(index) ? " ▼" : " ▶"}</span>
                                             </div>
                                         </td>
                                     </tr>
                                     {activeCurriculum.includes(index) && (
                                         <tr className="curriculum-content-show">
                                             <td>
-                                                {curriculum.contentList.map(
-                                                    (content) => (
-                                                        <div
-                                                            key={content.id}
-                                                            className="item-content"
-                                                            onClick={
-                                                                clickContent
-                                                            }
-                                                        >
-                                                            <span className="item-content-title">
-                                                                {content.title}
-                                                            </span>
-                                                        </div>
-                                                    )
-                                                )}
+                                                {curriculum.contentList.map((content) => (
+                                                    <div key={content.id} className="item-content">
+                                                        <span className="item-content-title">{content.title}</span>
+                                                        <span className="item-content-link">
+                                                            {content.type === "MATERIAL" ? (
+                                                                <Download
+                                                                    onClick={() =>
+                                                                        clickContent({
+                                                                            curriculumId: curriculum.id,
+                                                                            contentId: content.id,
+                                                                            contentType: content.type,
+                                                                        })
+                                                                    }
+                                                                />
+                                                            ) : content.type === "VIDEO" ? (
+                                                                <Play
+                                                                    onClick={() =>
+                                                                        clickContent({
+                                                                            curriculumId: curriculum.id,
+                                                                            contentId: content.id,
+                                                                            contentType: content.type,
+                                                                        })
+                                                                    }
+                                                                />
+                                                            ) : null}{" "}
+                                                        </span>
+                                                    </div>
+                                                ))}
                                             </td>
                                         </tr>
                                     )}
-                                </>
+                                </React.Fragment>
                             ))}
                         </tbody>
                     </table>
