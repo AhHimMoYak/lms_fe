4519d224baf5d35ac9ddb54ea84d4b38ddc75aad
Dowon <tnstjs1919@gmail.com>
2024-10-30 18:58:40 +0900

    mod : 수강 신청시 오류가 있으면 알림해주는 기능 추가

--- src/pages/Main/VideoExplain.jsx
+++ src/pages/Main/VideoExplain.jsx
@@ -1,49 +1,39 @@
-import {useState, useEffect} from "react";
-import {useParams} from "react-router-dom"; // useParams 훅 가져오기
+import {useState, useEffect, useCallback} from "react";
+import {useParams} from "react-router-dom";
 import useAxios from "../../hooks/api/useAxios.jsx";
 import "/src/styles/Main/VideoExplain.css";
 
 function VideoExplain() {
-    const {data, error, fetchData, isLoading} = useAxios(); // isLoading 추가
+    const {data: courseData, error, fetchData: fetchCourseData, isLoading} = useAxios();
+    const {data: enrollId, fetchData: fetchEnrollId} = useAxios();
     const {data: enrollment, error: enrollmentError, fetchData: fetchEnrollment} = useAxios();
+
     const {courseId} = useParams();
     const [activeCurriculum, setActiveCurriculum] = useState(null);
-    const [isEnrolled, setIsEnrolled] = useState(false);
+    const [isEnrolled, setIsEnrolled] = useState();
 
-    // API 호출
     useEffect(() => {
-        fetchData(`/course/${courseId}`, "get");
+        fetchCourseData(`/course/${courseId}`, "get");
     }, [courseId]);
 
-    // API 응답 데이터 로깅
     useEffect(() => {
-        if (data) {
-            console.log("API response:", data); // 응답 데이터 구조 확인
+        if (courseData) {
+            fetchEnrollId(`/course/${courseId}/enrollId`, "GET");
+            console.log("API response:", courseData);
         }
-    }, [data]);
+    }, [courseData]);
 
     useEffect(() => {
-        if (enrollment) {
-            if (isEnrolled) {
-                alert("수강신청이 취소되었습니다.");
-                setIsEnrolled(false);
-            } else{
-                alert("수강신청이 완료되었습니다.");
-                setIsEnrolled(true);
-            }
+        if (enrollId) {
+            setIsEnrolled(enrollId.id !== null);
         }
-    }, [enrollment]);
+    }, [enrollId]);
 
     useEffect(() => {
         if (enrollmentError) {
-            console.error("Enrollment Error:", enrollmentError);
-            if (enrollmentError.response && enrollmentError.response.status !== 200 && enrollmentError.response.status !== 201) {
-                console.error("Response Data:", enrollmentError.response.data);
-            }
             alert(`${enrollmentError.response.data}. 담당자에게 문의해주세요`);
         }
     }, [enrollmentError]);
-
 
     const scrollToSection = (sectionId) => {
         const section = document.getElementById(sectionId);
@@ -58,6 +48,20 @@ function VideoExplain() {
         );
     };
 
+    const handleEnrollmentToggle = useCallback(() => {
+        if (isEnrolled) {
+            if (window.confirm("수강신청을 취소하시겠습니까?")) {
+                fetchEnrollment(`/course/${courseId}/enroll`, "DELETE");
+                setIsEnrolled(false);
+            }
+        } else {
+            if (window.confirm("수강신청을 하시겠습니까?")) {
+                fetchEnrollment(`/course/${courseId}/enroll`, "GET");
+                setIsEnrolled(true);
+            }
+        }
+    }, [isEnrolled, courseId, fetchEnrollment]);
+
     if (isLoading) {
         return <div>Loading...</div>;
     }
@@ -66,43 +70,30 @@ function VideoExplain() {
         return <div>Error: {error.message}</div>;
     }
 
-    if (!data) {
+    if (!courseData) {
         return <div>데이터를 불러올 수 없습니다.</div>;
     }
 
-    const handleEnrollmentToggle = () => {
-        if (isEnrolled) {
-            if (window.confirm("수강신청을 취소하시겠습니까?")) {
-                fetchEnrollment(`/course/${courseId}/enroll`, "DELETE")
-            }
-        } else {
-            if (window.confirm("수강신청을 하시겠습니까?")) {
-                fetchEnrollment(`/course/${courseId}/enroll`, "GET")
-            }
-        }
-    };
-
-    const enrollmentPeriod = new Date(data.endDate).setHours(23, 59, 59, 999) >= new Date();
+    const enrollmentPeriod = new Date(courseData.endDate).setHours(23, 59, 59, 999) >= new Date();
 
     return (
         <div className="container">
             <div className="lecture-info">
                 <div className="lecture-card">
                     <div className="lecture-details">
-                        <h2>{data.title}</h2>
-                        <p>{data.tutor} 강사</p>
+                        <h2>{courseData.title}</h2>
+                        <p>{courseData.tutor} 강사</p>
                     </div>
                 </div>
                 {enrollmentPeriod ? (
-                        <button className="course-provide-button" onClick={handleEnrollmentToggle}>
-                            {isEnrolled ? "수강 취소" : "수강 신청"}
-                        </button>
-                    )
-                    : (
-                        <div className="course-provide-button-disabled">
-                            수강신청 기간이 아닙니다
-                        </div>
-                    )}
+                    <button className="course-provide-button" onClick={handleEnrollmentToggle}>
+                        {isEnrolled ? "수강 취소" : "수강 신청"}
+                    </button>
+                ) : (
+                    <div className="course-provide-button-disabled">
+                        수강신청 기간이 아닙니다
+                    </div>
+                )}
             </div>
 
             <div className="lecture-details-section">
@@ -110,20 +101,10 @@ function VideoExplain() {
                     <h2>교육 소개</h2>
                 </div>
                 <div className="course-tabs">
-                    <button onClick={() => scrollToSection("learning-goal")}>
-                        소개
-                    </button>
-                    <button onClick={() => scrollToSection("chapter-info")}>
-                        커리큘럼
-                    </button>
-                    <button onClick={() => scrollToSection("instructor-intro")}>
-                        강사진 소개
-                    </button>
-                    <button
-                        onClick={() => scrollToSection("completion-criteria")}
-                    >
-                        평가
-                    </button>
+                    <button onClick={() => scrollToSection("learning-goal")}>소개</button>
+                    <button onClick={() => scrollToSection("chapter-info")}>커리큘럼</button>
+                    <button onClick={() => scrollToSection("instructor-intro")}>강사진 소개</button>
+                    <button onClick={() => scrollToSection("completion-criteria")}>평가</button>
                 </div>
 
                 <div id="learning-goal" className="learning-goal">
@@ -132,15 +113,15 @@ function VideoExplain() {
                         <tbody>
                         <tr>
                             <th>강의명</th>
-                            <td>{data.title}</td>
+                            <td>{courseData.title}</td>
                         </tr>
                         <tr>
                             <th>강사명</th>
-                            <td>{data.tutor}</td>
+                            <td>{courseData.tutor}</td>
                         </tr>
                         <tr>
                             <th>교육 소개</th>
-                            <td>{data.introduction}</td>
+                            <td>{courseData.introduction}</td>
                         </tr>
                         </tbody>
                     </table>
@@ -148,16 +129,10 @@ function VideoExplain() {
 
                 <div id="chapter-info" className="chapter-info">
                     <h3>- 커리큘럼</h3>
-                    {data.curriculumList && data.curriculumList.length > 0 ? (
-                        data.curriculumList.map((curriculum, index) => (
-                            <div
-                                key={curriculum.id}
-                                className="curriculum-section"
-                            >
-                                <div
-                                    className="section-header"
-                                    onClick={() => toggleCurriculum(index)}
-                                >
+                    {courseData.curriculumList && courseData.curriculumList.length > 0 ? (
+                        courseData.curriculumList.map((curriculum, index) => (
+                            <div key={curriculum.id} className="curriculum-section">
+                                <div className="section-header" onClick={() => toggleCurriculum(index)}>
                                     <h4>{curriculum.title}</h4>
                                     <p className="curriculum-detail-toggle">
                                         {activeCurriculum === index ? "▼" : "▶"}
@@ -165,18 +140,15 @@ function VideoExplain() {
                                 </div>
                                 {activeCurriculum === index && (
                                     <div className="section-content">
-                                        {curriculum.contentList.map(
-                                            (content) => {
-                                                return (
-                                                    <div key={content.id}>
-                                                        <span>
-                                                            {content.id}{" "}
-                                                            {content.title}
-                                                        </span>
-                                                    </div>
-                                                );
-                                            }
-                                        )}
+                                        {curriculum.contentList.map((content) => {
+                                            return (
+                                                <div key={content.id}>
+                                                    <span>
+                                                        {content.id} {content.title}
+                                                    </span>
+                                                </div>
+                                            );
+                                        })}
                                     </div>
                                 )}
                             </div>
