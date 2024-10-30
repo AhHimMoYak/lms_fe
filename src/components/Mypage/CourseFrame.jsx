806b31b4cbda631e2abf6d5de0a3dd1d7dd71a10
sora0319 <sojeoung536985@gmail.com>
2024-10-30 10:19:57 +0900

    style : 나의 코스에 있는 코스 탭 스타일 변환

--- src/components/Mypage/CourseFrame.jsx
+++ src/components/Mypage/CourseFrame.jsx
@@ -1,33 +1,51 @@
 import { NavLink, Outlet } from "react-router-dom";
 import "../../styles/Mypage/CourseFrame.css";
+import { patch } from "@mui/material";
 
 function CourseFrame() {
+    const checkIsActive = (path) => {
+        return location.pathname.includes(path);
+    };
+
+    const checkIsContent = (path) => {
+        const regex = new RegExp(`^/mypage/course/\\d+(/${path})?$`);
+        return regex.test(location.pathname);
+    };
+
     return (
         <div className="course-frame">
             <div className="course-tab-frame">
-                <h2>나의 코스</h2>
-                <div className="tabs">
+                <div className="course-frame-title">나의 코스</div>
+                <div className="course-tabs">
                     <NavLink
                         to="contents"
-                        className={({ isActive }) => (isActive ? "active" : "")}
+                        className={() =>
+                            checkIsContent("contents")
+                                ? "course_tab_active"
+                                : ""
+                        }
                     >
                         강의
                     </NavLink>
                     <NavLink
                         to="live"
-                        className={({ isActive }) => (isActive ? "active" : "")}
+                        className={() =>
+                            checkIsActive("live") ? "course_tab_active" : ""
+                        }
                     >
                         라이브
                     </NavLink>
                     <NavLink
                         to="qna"
-                        className={({ isActive }) => (isActive ? "active" : "")}
+                        className={() =>
+                            checkIsActive("qna") ? "course_tab_active" : ""
+                        }
                     >
                         QnA
                     </NavLink>
                 </div>
 
-                <div className="tab-content">
+                <div className="course-tab-content">
                     <Outlet />
                 </div>
             </div>
