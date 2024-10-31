9978b826d4f033e37d402368831bab15725f18aa
nowgnodeel123 <nowgnodeel123@gmail.com>
2024-10-31 14:45:58 +0900

    refactor : 카드 리스트 간격 수정

--- src/components/Main/MainCourseCardList.jsx
+++ src/components/Main/MainCourseCardList.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import Slider from "react-slick";
 import useAxios from "../../hooks/api/useAxios.jsx";
 import "../../styles/Main/MainCourseList.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -16,8 +16,8 @@ function shuffleArray(array) {
 
 function MainCourseCardList() {
     const [courses, setCourses] = useState([]);
-    const [loading, setLoading] = useState(true); // 초기에는 로딩 상태로 설정
-    const { fetchData, data } = useAxios();
+    const [loading, setLoading] = useState(true);
+    const {fetchData, data} = useAxios();
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -37,36 +37,36 @@ function MainCourseCardList() {
     };
 
     const sliderSettings = {
-        dots: false, // 아래 점 네비게이션 숨기기
-        infinite: true, // 무한 반복 슬라이딩
-        speed: 500, // 슬라이딩 전환 속도 (500ms)
-        slidesToShow: 4.5, // 한 번에 보여줄 슬라이드 수를 설정
-        slidesToScroll: 0.1, // 물 흐르듯 부드럽게 이동하도록 설정 (1보다 작게 설정하여 부드러운 전환)
-        autoplay: true, // 자동 슬라이딩 활성화
-        autoplaySpeed: 20, // 슬라이딩 간의 시간 간격 (0.02초마다 이동)
-        cssEase: "linear", // 부드러운 이동을 위해 linear 이징 효과 사용
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4.5,
+        slidesToScroll: 0.15,
+        autoplay: true,
+        autoplaySpeed: 15,
+        cssEase: "linear",
         pauseOnHover: false,
-        arrows: false, // 좌우 화살표 숨기기
+        arrows: false,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 0.1, // 여기서도 부드러운 전환 설정
+                    slidesToScroll: 0.1,
                 },
             },
             {
                 breakpoint: 768,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 0.1, // 여기서도 부드러운 전환 설정
+                    slidesToScroll: 0.1,
                 },
             },
             {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 0.1, // 여기서도 부드러운 전환 설정
+                    slidesToScroll: 0.1,
                 },
             },
         ],
@@ -81,20 +81,23 @@ function MainCourseCardList() {
             ) : (
                 <Slider {...sliderSettings} className="course-slider">
                     {courses.map((course) => (
-                        <div
-                            key={course.id}
-                            className="course-main-card"
-                            onClick={() => clickDetailCourse(course.id)}
-                        >
+                        <>
                             <div
-                                className="course-main-card-header"
-                                style={{ backgroundColor: getRandomColor() }}
-                            ></div>
-                            <div className="course-main-card-content">
-                                <h3 className="course-main-card-title">{course.title}</h3>
-                                <h4 className="course-main-card-tutor">{course.tutor}</h4>
+                                key={course.id}
+                                className="course-main-card"
+                                onClick={() => clickDetailCourse(course.id)}
+                            >
+                                <div
+                                    className="course-main-card-header"
+                                    style={{backgroundColor: getRandomColor()}}
+                                ></div>
+                                <div className="course-main-card-content">
+                                    <h3 className="course-main-card-title">{course.title}</h3>
+                                    <h4 className="course-main-card-tutor">{course.tutor}</h4>
+                                </div>
                             </div>
-                        </div>
+                            <div></div>
+                        </>
                     ))}
                 </Slider>
             )}
@@ -102,7 +105,6 @@ function MainCourseCardList() {
     );
 }
 
-// 색상을 랜덤으로 선택하기 위한 함수
 function getRandomColor() {
     const colors = [
         "#3f51b5",
