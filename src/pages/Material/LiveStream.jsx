31d7b1ae867bfc0cdd8da6fc3a7e57a06fd82ee9
sora0319 <sojeoung536985@gmail.com>
2024-10-31 16:50:18 +0900

    feat : 모달 기능 추가

--- src/pages/Material/LiveStream.jsx
+++ src/pages/Material/LiveStream.jsx
@@ -6,15 +6,14 @@ import HLSPlayer from "../../components/Material/HLSPlayer";
 import useAxios from "../../hooks/api/useAxios.jsx";
 
 import "../../styles/Material/LiveStream.css";
+import Modal from "../../components/Modal.jsx";
+import { decodeTokenTutor } from "../../authentication/decodeTokenTutor.jsx";
 
 function LiveStream() {
     const liveUrl = "http://172.16.10.251:8085/hls/";
 
-    const {
-        fetchData: fetchLiveInfo,
-        data: liveInfo,
-        error: liveInfoError,
-    } = useAxios();
+    const { fetchData: fetchLiveInfo, data: liveInfo } = useAxios();
+
     useEffect(() => {
         fetchLiveInfo(`/live/${streamKey}`, "get");
     }, []);
@@ -24,21 +23,27 @@ function LiveStream() {
     const { streamKey } = useParams();
 
     const navigate = useNavigate();
-    const handleNavigateToCourse = () =>
-        navigate(`/mypage/course/${liveInfo.courseId}`);
+    const handleNavigateToCourse = () => navigate(`/mypage/course/${liveInfo.courseId}`);
 
     const [chatOpen, setChatOpen] = useState(true);
     const [viewer, setViewer] = useState(0);
 
     if (liveInfo) console.log(liveInfo);
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalId, setModalId] = useState(null);
+
+    const openModal = (event) => {
+        setModalId(event.target.id);
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
     return (
-        <div
-            style={{
-                backgroundColor: "purple",
-            }}
-            className="stream-page"
-        >
+        <div className="stream-page">
             <div className="stream-container">
                 <div className="video-container">
                     {/*<VideoPlayer url={liveUrl} streamKey={streamKey} type="m3u8" />*/}
@@ -55,6 +60,21 @@ function LiveStream() {
                             <h2>{liveInfo.title}</h2>
                             <p>{liveInfo.instructor}</p>
                             <p>Viewers: 123</p>
+
+                            {decodeTokenTutor() ? (
+                                <div>
+                                    <button className="open-modal-button" id="send" onClick={openModal}>
+                                        퀴즈 목록 및 전송 모달
+                                    </button>
+                                    {/* {isModalOpen && <Modal closeModal={closeModal} buttonId={modalId} />} */}
+                                    <button className="open-modal-button" id="participation" onClick={openModal}>
+                                        정답률 보는 모달
+                                    </button>
+                                    {isModalOpen && <Modal closeModal={closeModal} buttonId={modalId} />}
+                                </div>
+                            ) : (
+                                <></>
+                            )}
                         </div>
                     ) : (
                         <div className="stream-info">
