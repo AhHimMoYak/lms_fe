2ee9137f7b4c13b3feced4c14e663f34ebcea2e9
sora0319 <sojeoung536985@gmail.com>
2024-11-12 16:25:10 +0900

    feat : hosting url 조건 추가

--- src/api-config.jsx
+++ src/api-config.jsx
@@ -2,8 +2,12 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-if(hostname === "localhost") {
+if (hostname === "localhost") {
     backendHost = "http://localhost:8080/api/v1";
+}
+
+if (hostname === "ahimmoyak.click") {
+    backendHost = "https://ahimmoyak.click";
 }
 
 export const API_BASE_URL = `${backendHost}`;
