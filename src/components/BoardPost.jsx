d729289685836e95b133bb7edf0e00f4c0120dfc
gomar <ju79318@naver.com>
2024-11-15 16:39:30 +0900

    mod : courseProvideId, type을 useParams로 받기, request 보내는 변수 추가(courseProvideId,type), navigate 변경(랜더링이 변화하여), 페이지 구성 변경

--- src/components/BoardPost.jsx
+++ src/components/BoardPost.jsx
@@ -1,48 +1,44 @@
 import {useEffect, useState} from 'react';
-import {useLocation, useNavigate, useParams} from 'react-router-dom';
+import {useNavigate, useParams} from 'react-router-dom';
 import useAxios from "../hooks/api/useAxios.jsx";
-import "../styles/Mypage/QnADetailed.css";
+import "../styles/Mypage/Board.css";
 
 function BoardPost() {
-    const {type} = useParams();
+    const {courseProvideId,type} = useParams();
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
     const {data, fetchData} = useAxios();
     const navigate = useNavigate();
-    const {state} = useLocation();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
         const requestDTO = {
             institutionId : "2",
-            courseProvideId : state.courseProvideId.toString(),
+            courseProvideId : courseProvideId,
             userName:"난중에고치기",
-            type:state.type,
+            type:type,
             title: title,
             content: content
         };
-        console.log(state.type);
         fetchData(`https://api.ahimmoyak.click/board/v1/courseProvide`, "POST", requestDTO);
     };
 
     useEffect(() => {
         if (data) {
             alert("글 작성 성공!");
-            navigate(`/test`,{state:{courseProvideId: state.courseProvideId, type:state.type}});
-
+            navigate(`/mypage/course/${courseProvideId}/board/${type}`);
         }
     }, [data]);
 
     const handleList = () => {
-        navigate(`/test`,{state:{courseProvideId: state.courseProvideId, type:state.type}});
+        navigate(`/mypage/course/${courseProvideId}/board/${type}`);
     }
 
 
     return (
-        <div className="qna-container">
-            <div className='qna-post-title'>Q&A 게시물 작성</div>
-            <form className="qna-form" onSubmit={handleSubmit}>
+        <div className="board-container">
+            <div className='board-post-title'>Q&A 게시물 작성</div>
+            <form className="board-form" onSubmit={handleSubmit}>
                 <div className="title-box">
                     <input
                         type="text"
@@ -52,16 +48,16 @@ function BoardPost() {
                         required
                     />
                 </div>
-                <div className="content-container">
+                <div className="board-content-container">
                     <textarea
-                        className="content-box"
+                        className="board-content-box"
                         placeholder="내용을 입력해주세요"
                         value={content}
                         onChange={(e) => setContent(e.target.value)}
                         required
                     />
                 </div>
-                <button type="submit" className="qna-submit-button">
+                <button type="submit" className="board-submit-button">
                     작성완료
                 </button>
                 <button className="qna-back-button" onClick={handleList}>
