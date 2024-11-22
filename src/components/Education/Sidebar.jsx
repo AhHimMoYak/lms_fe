68124afffc7ee9e04192b924d8477ca08ee295a8
gomar <ju79318@naver.com>
2024-11-22 12:03:06 +0900

    mod : QnA => 게시글로 수정

--- src/components/Education/Sidebar.jsx
+++ src/components/Education/Sidebar.jsx
@@ -33,8 +33,8 @@ const Sidebar = () => {
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/education/qna" className={() => (checkIsActive("/education/qna") ? "sidebar_active" : "")}>
-                            QnA
+                        <NavLink to="/education/board/Notice" className={() => (checkIsActive("/education/board/Notice") ? "sidebar_active" : "")}>
+                            게시글
                         </NavLink>
                     </li>
                     <li>
