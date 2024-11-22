ce0cf367d458e1fe90508e33ed005c596f36168d
tiktokyi <tiktokyi@gmail.com>
2024-11-22 21:28:54 +0900

    css : contentsCard css 변경

--- lms-fe/src/components/Mypage/CreateCourse_v2/GetContents.jsx
+++ lms-fe/src/components/Mypage/CreateCourse_v2/GetContents.jsx
@@ -2,12 +2,9 @@ import { MdOutlineClose } from "react-icons/md";
 import "../../../styles/Mypage/CreateCourse_v2/AddContents.css";
 
 function GetContents({ idx, originalFileName, uploadedAt, content, onDelete }) {
-  console.log(content.contentId);
-
   const handleDeleteClick = () => {
     if (window.confirm(`${originalFileName}을 삭제하시겠습니까?`)) {
       onDelete(content.contentId, idx);
-      console.log(content.contentId);
     }
   };
 
@@ -19,19 +16,13 @@ function GetContents({ idx, originalFileName, uploadedAt, content, onDelete }) {
       <div className="getContentsContainer">
         <div className="idxContainer">{idx}</div>
         <div className="fileInfoContainer">
-          <div className="fileNameContainer">{originalFileName}</div>
-          <div className="contentTitleContainer">
-            {content.contentTitle}
-          </div>{" "}
-          {/* contentTitle 추가 */}
-          <div className="contentTypeContainer">{content.contentType}</div>{" "}
-          {/* contentType 추가 */}
-          <div className="s3UrlContainer">
+          <div className="contentTitleContainer">{content.contentTitle}</div>
+          <div className="fileNameContainer">파일명 : {originalFileName}</div>
+          <div className="contentTypeContainer">
             <a href={content.s3Url} target="_blank" rel="noopener noreferrer">
-              {content.s3Url}
+              {content.contentType}
             </a>
-          </div>{" "}
-          {/* s3Url 추가, 링크로 클릭 가능하게 */}
+          </div>
           <div className="dateContainer">{uploadedAt}</div>
         </div>
       </div>
