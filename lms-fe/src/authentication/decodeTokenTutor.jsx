974d78f0783134991914fbaa762500e4c1f4669a
nowgnodeel <nowgnodeel123@gmail.com>
2024-11-28 09:36:12 +0900

    mod : user role tutor -> manager로 변경

--- lms-fe/src/authentication/decodeTokenTutor.jsx
+++ lms-fe/src/authentication/decodeTokenTutor.jsx
@@ -4,7 +4,13 @@ export function decodeTokenTutor() {
     try {
         const token = localStorage.getItem("access");
         const claims = jwtDecode(token);
-        return claims.tutor;
+        console.log(claims.role);
+        if (claims.role === "MANAGER") {
+            return "manager";
+        } else {
+            return null;
+        }
+        // return claims.role;
     } catch (err) {
         console.error("토큰 디코딩 실패:", err.message);
         return null;
